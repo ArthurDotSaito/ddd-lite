7617c5bcfe504b6b5013f0d6ace5b925e332513d
Arthur Saito <arthur_saito1998@hotmail.com>
2024-06-07 21:20:21 -0300

    +validation

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,35 +1,39 @@
 class Customer {
   _id: string;
-  _name: string;
-  _email: string;
+  _name: string = "";
+  _email: string = "";
+  _active: boolean = true;
 
   constructor(id: string, name: string, email: string) {
     this._id = id;
     this._name = name;
     this._email = email;
+    this.validate();
   }
 
-  get id(): string {
-    return this._id;
+  validate() {
+    if (this._id.length == 0) {
+      throw new Error("Id is required");
+    }
+
+    if (this._name.length == 0) {
+      throw new Error("Name is required");
+    }
+
+    if (this._email.length == 0) {
+      throw new Error("Email is required");
+    }
   }
 
-  get name(): string {
-    return this._name;
+  changeName(name: string) {
+    this._name = name;
   }
 
-  get email(): string {
-    return this._email;
+  activate() {
+    this._active = true;
   }
 
-  set id(value: string) {
-    this._id = value;
-  }
-
-  set name(value: string) {
-    this._name = value;
-  }
-
-  set email(value: string) {
-    this._email = value;
+  deactivate() {
+    this._active = false;
   }
 }
